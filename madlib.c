186ec077a010e75cd36e25eb0af583a06cbac21b
toulouce <toulouce.h@gmail.com>
2025-08-19 23:16:46 +0800

    Update madlib.c
    It's now deprecated, not working, too many bugs to fix will rewrite later

--- madlib.c
+++ madlib.c
@@ -5,11 +5,15 @@
 char Continue;
 int main(void)
 {
+    char couleur[50];
+    char nom[50];
+    char beverage[50];
     printf("***MADLIB v1.0.0 ***\n");
 restart:    
     printf("Enter Y to continue N to not: ");
-    scanf("%c", &Continue);
-    toupper(Continue);
+    scanf(" %c", &Continue);
+    while (getchar() != '\n');
+    Continue = toupper(Continue);
     switch (Continue)
     {
     case 'Y':
@@ -20,22 +24,24 @@ restart:
         goto restart;
     }
 game:
-    char *couleur;
-    char *nom;
-    char *beverage;
     printf("\nLet's start shall we?\n");
+    printf("Let's begin!\n");
 reprendre:
     printf("Enter a Colour: ");
-    scanf("%s", couleur);
+    scanf(" %s", couleur);
+    while (getchar() != '\n');  
     printf("\nEnter a name: ");
-    scanf("%s", nom);
+    scanf(" %s", nom);
+    while (getchar() != '\n');
     printf("\nEnter a drink: ");
-    scanf("%s", beverage);
+    scanf(" %s", beverage);
+    while (getchar() != '\n');
     printf("%s spilled %s on his %s jacket\n and he is angry now\n", nom, beverage, couleur);
 reprendre1:    
     printf("Do you want to continue (Y/N): ");
-    scanf("%c" &Continue);
-    toupper(Continue);
+    scanf(" %c", &Continue);
+    while (getchar() != '\n');
+    Continue = toupper(Continue);
     if(Continue == 'Y')
         goto reprendre;
     else if(Continue == 'N')
