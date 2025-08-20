f9a83356e315a4bd57f5bc02dccc63c40e2eb737
toulouce <toulouce.h@gmail.com>
2025-08-20 22:35:05 +0800

    Create swiftMadlib.swift
    Very simple madlib game will add features later or not who knows

---
+++ swiftMadlib.swift
@@ -0,0 +1,11 @@
+//Swift Madlib
+
+@main
+
+print("*** Madlibs Game in Swift ***")
+print("Enter a name: ", terminator:""); var nom = readLine()
+print("Enter a drink/beverage: ", terminator:""); var beverage = readLine()
+print("Enter a colour: ", terminator:""); var couleur = readLine()
+print("\(nom) spilled \(beverage) on their \(couleur) jacket and they're furious now.")
+
+                                                    
