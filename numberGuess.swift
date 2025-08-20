c3def417d920c50ebae16e5f42802169a7213d91
toulouce <toulouce.h@gmail.com>
2025-08-20 23:04:56 +0800

    Update numberGuess.swift
    Bugfix

--- numberGuess.swift
+++ numberGuess.swift
@@ -1,13 +1,18 @@
 //Number Guessing game
-
+import Foundation
 let number = Int.random(in: 1...100)
 print("Guess the number in between 1 to 100")
 var guessCount = 0
-var guess: Int
+var guess: Int = 0
 repeat {
   guessCount += 1
-  print("Guess\(guessCount): ", terminator:"")
-  var input = readline(); guess = Int(input)
+  print("Guess \(guessCount): ", terminator:"")
+  if let input = readline(), let inputnum = Int(input) {
+      guess = inputnum
+  } else {
+      print("Please enter a valid number.")
+      continue
+  }
   if guess < number {
     print("Guess is too LOW!")
   } else if guess == number {
