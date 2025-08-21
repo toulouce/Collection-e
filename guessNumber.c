1a6f5f17e3948df31a356ee75f713889dde027bb
toulouce <toulouce.h@gmail.com>
2025-08-21 11:59:53 +0800

    Create guessNumber.c

---
+++ guessNumber.c
@@ -0,0 +1,28 @@
+//Number Guessing Game
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+  srand(time(NULL));
+  while(1) {
+  int num = (rand() % 100) + 1;
+  char Continue;
+  printf("Do you want to continue(Y/N): ");
+  scanf(" %c", &Continue);
+  if(Continue == 'Y') {
+  printf("Guess the number in between 1-100");
+  int guess_count = 1;
+  do {
+      int guess = 0;
+      printf("Guess %c: ", guess_count);
+      scanf(" %d", &guess);
+      if(guess == NULL) { printf("Please enter a valid number"); continue; }
+      if(guess < num) printf("You guessed too low.");
+      else if(guess > num) printf("You guessed too high.");
+      else printf("You guessed correct!");
+    } while (guess != num);
+   } else break; 
+  }
+  return 0;
+}
