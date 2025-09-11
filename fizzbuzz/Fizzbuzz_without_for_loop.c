62ef488c530cc33f5aca913343fecdaa48955ea4
toulouce <toulouce.h@gmail.com>
2025-09-11 19:28:00 +0800

    Now include Fizzbuzz() function that uses ternerary operator still has the fizzbuzz() function with switch case

--- fizzbuzz/Fizzbuzz_without_for_loop.c
+++ fizzbuzz/Fizzbuzz_without_for_loop.c
@@ -1,5 +1,6 @@
 //FIZZBUZZ without for loop and else/if statement
 #include <stdio.h>
+#include <stdlib.h>
 
 int i = 1;
 int n = 0;
@@ -12,13 +13,18 @@ void fizzbuzz(int n){
       i % 15 == 0 ? printf("FizzBuzz\n") : i % 3 == 0 ? printf("Fizz\n") : i % 5 == 0 ? printf("Buzz\n"): printf("%d\n", i);
       i++;
       n--;
-      fizzbuzz(n);
+      fizzbuzz(n); i % 15 == 0 ? printf("FizzBuzz\n") : i % 3 == 0 ? printf("Fizz\n") : i % 5 == 0 ? printf("Buzz\n"): printf("%d\n", i);
     }
+}
+void Fizzbuzz(int n){
+    n <= 0 ? exit(0) : i % 15 == 0 ? printf("FizzBuzz\n") : i % 3 == 0 ? printf("Fizz\n") : i % 5 == 0 ? printf("Buzz\n"): printf("%d\n", i);
+    i++;
+    Fizzbuzz(n - 1);
 }
 
 int main(void){
     printf("Enter a number: ");
     scanf("%d", &n);
-    fizzbuzz(n);
+    Fizzbuzz(n);
     return 0;
 }
