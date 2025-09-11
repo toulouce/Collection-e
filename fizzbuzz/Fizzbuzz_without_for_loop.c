af2a1a4cbb34a8f866a2b392e1ecc388c8661766
toulouce <toulouce.h@gmail.com>
2025-09-11 19:20:35 +0800

    FIXED THE SEG_FAULT PROBLEM

--- fizzbuzz/Fizzbuzz_without_for_loop.c
+++ fizzbuzz/Fizzbuzz_without_for_loop.c
@@ -2,19 +2,21 @@
 #include <stdio.h>
 
 int i = 1;
+int n = 0;
+
 void fizzbuzz(int n){
-    switch(i){
+    switch(n){
     case 0:
         return;
     default:
       i % 15 == 0 ? printf("FizzBuzz\n") : i % 3 == 0 ? printf("Fizz\n") : i % 5 == 0 ? printf("Buzz\n"): printf("%d\n", i);
       i++;
-      fizzbuzz(n-1);
+      n--;
+      fizzbuzz(n);
     }
 }
 
 int main(void){
-    int n = 0;
     printf("Enter a number: ");
     scanf("%d", &n);
     fizzbuzz(n);
