9b9cc23144eddf744a2c0178c9e81116d9df4c7e
toulouce <toulouce.h@gmail.com>
2025-09-15 16:29:02 +0800

    Update helloworld.asm

--- as/helloworld.asm
+++ as/helloworld.asm
@@ -1,20 +1,20 @@
-.global _main
+.global _start
 
 .text
-_main:
+_start:
     ; Hello World program for AArch64 Darwin
     mov x0, #1               ; wtf do you mean ldr is not valid kind
-    adr x1, _HelloWorld
+    adr x1, msg
     mov x2, #13
     mov x16, #4
-    svc #0x80
+    svc #0
 
     mov x0, #0
     mov x16, #1
     svc #0x80
 
 .data
-_HelloWorld:
+msg:
     .ascii "Hello, World!\n"
 
     
