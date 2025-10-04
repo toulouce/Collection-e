91a350b56427ada0114aade0a049a322839ee2e8
toulouce <toulouce.h@gmail.com>
2025-10-04 15:46:22 +0800

    some documentation?

--- solutions/c/collatz-conjecture/1/collatz_conjecture.c
+++ solutions/c/collatz-conjecture/1/collatz_conjecture.c
@@ -1,17 +1,18 @@
 #include "collatz_conjecture.h"
 
 int steps(int start){
-    int count = 0;
-    if(start > 0){
+    int count = 0;  //Count of Steps to reach 0
+    if(start > 0){  // Check if the starting value is valid
+    //Break out of the loop if the val is equal to 1
     while (start != 1){
-        if(start % 2 == 0){
+        if(start % 2 == 0){ //Check if the value is even or odd
             start /= 2;
         } else {
             start = (start * 3) + 1;
         }
          count++;
     }
-    } else if(start == 0) return -1;
-    else return -1;
+    } else if(start == 0) return -1; //Start value is 0
+    else return -1; //Invalid Value Entered
     return count;
 }
